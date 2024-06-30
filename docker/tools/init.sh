b71c28ec723dab10b293716fdb245564115b8c64
iv-lite <ibekwe.victor@gmail.com>
2024-06-30 11:22:40 +0200

    Add - improved onboarding process

--- docker/tools/init.sh
+++ docker/tools/init.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env sh
 
 if ! [ -f /.dockerenv ]; then
-    docker compose run app init.sh
+  docker compose run app init.sh --rm
 fi
 
 if [ -z $APP_NAME ]; then
-    echo "Please provide a name for your project by setting APP_NAME environment variable."
-    exit 1
+  echo "Please provide a name for your project by setting APP_NAME environment variable."
+  exit 1
 fi
 
 if [ -f package.json ]; then
-    echo "Project already initialized"
-    exit 1
+  echo "Project already initialized"
+  exit 1
 fi
 
-ng new $1 --skip-git
-# cp -a /app/$1/. .
-# rm -rf $1
+ng new $APP_NAME --skip-git
+
+cp -a /app/$APP_NAME/. /app
+rm -rf $APP_NAME
+
+if [ $DEBUG_TOOLS = true ]; then
+  exit 0
+fi
+
+rm -rf /app/.git
+cd /app
+git init
--- docker/tools/ng-dev.sh
+++ docker/tools/ng-dev.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env sh
-ng serve --host 0.0.0.0 --disableHostCheck
+
+docker_path="$(dirname $0)/.."
+
+if ! [ -f /.dockerenv ]; then
+  cd "${docker_path}"
+  docker compose run app ng-dev.sh --rm
+  exit 0
+fi
+
+if ! [ -f /app/angular.json ]; then
+  echo "Project not initialized..."
+  init.sh
+fi
+
+ng serve --host 0.0.0.0 --disable-host-check
